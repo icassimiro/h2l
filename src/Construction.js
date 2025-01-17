0cb8da813acb276d7608de2ad1e38f0e2439b7cc
icassimiro <icassimiro@hotmail.com>
2025-01-17 14:20:23 -0300

    css

--- src/Construction.js
+++ src/Construction.js
@@ -1,6 +1,7 @@
 
 import ipiranga from "./Images/ipiranga.png";
 import vila from "./Images/vila.png";
+import vilamobile from "./Images/vilamobile.png";
 import AwesomeSlider from "react-awesome-slider";
 import withAutoplay from "react-awesome-slider/dist/autoplay";
 import "react-awesome-slider/dist/styles.css";
@@ -9,14 +10,15 @@ const AutoplaySlider = withAutoplay(AwesomeSlider);
 
 export default function Construction() {
   return (
+    <div>
     <div className="divcarousel">
-      <AutoplaySlider play={true} cancelOnInteraction={false} interval={12000}>
+      <AutoplaySlider play={false} cancelOnInteraction={false} interval={12000}>
         <div className="slide">
           <img className="slideimg" src={vila} alt=""></img>
           <div className="columnap">
           
           <h2 className="titleap">Vila Prudente II</h2>
-          <hr className="hrr"></hr>
+        
           
           <h2 className="subtitleap">Localizado em São Paulo, este empreendimento de 28 pavimentos no sistema construtivo parede de concreto com 235 unidades, marca mais um importante passo em nossa trajetória, reforçando nossa parceria com a Metrocasa e reafirmando nosso compromisso em entregar qualidade, solidez e inovação em cada detalhe.</h2>
                <button className="projeto">VISUALIZAR PROJETO</button>
@@ -25,7 +27,7 @@ export default function Construction() {
         </div>
         <div className="slide">
           <img className="slideimg" src={ipiranga} alt=""></img>
-          <div className="columnap">
+           <div className="columnap">
           <h2 className="titleap">Alto do Ipiranga</h2>
           <hr className="hrr"></hr>
           <h2 className="subtitleap">Localizado em São Paulo, este projeto representa o inicio de uma importante parceria com a Metrocasa, e reflete nosso objetivo de construir estruturas sólidas e relacionamentos duradouros. 
@@ -33,11 +35,47 @@ export default function Construction() {
 Estamos orgulhosos de trabalhar ao lado de uma equipe dedicada para tornar este projeto uma realidade.</h2>
           <button className="projeto">VISUALIZAR PROJETO</button>
           </div>
+       
         </div>
+
+       
         
        
        
       </AutoplaySlider>
+    </div>
+    <div className="divcarouselm">
+      <AutoplaySlider play={false} cancelOnInteraction={false} interval={12000}>
+        <div className="slide">
+          <img className="slideimg" src={vilamobile} alt=""></img>
+          <div className="columnap">
+          
+          <h2 className="titleap">Vila Prudente II</h2>
+         
+          
+          <h2 className="subtitleap">Localizado em São Paulo, este empreendimento de 28 pavimentos no sistema construtivo parede de concreto com 235 unidades, marca mais um importante passo em nossa trajetória, reforçando nossa parceria com a Metrocasa e reafirmando nosso compromisso em entregar qualidade, solidez e inovação em cada detalhe.</h2>
+               <button className="projeto">VISUALIZAR PROJETO</button>
+          </div>
+          
+        </div>
+        <div className="slide">
+          <img className="slideimg" src={vilamobile} alt=""></img>
+           <div className="columnap">
+          <h2 className="titleap">Alto do Ipiranga</h2>
+          <hr className="hrr"></hr>
+          <h2 className="subtitleap">Localizado em São Paulo, este projeto representa o inicio de uma importante parceria com a Metrocasa, e reflete nosso objetivo de construir estruturas sólidas e relacionamentos duradouros. 
+
+Estamos orgulhosos de trabalhar ao lado de uma equipe dedicada para tornar este projeto uma realidade.</h2>
+          <button className="projeto">VISUALIZAR PROJETO</button>
+          </div>
+       
+        </div>
+       
+        
+       
+       
+      </AutoplaySlider>
+    </div>
     </div>
   );
 }
--- src/Texthl.js
+++ src/Texthl.js
@@ -1,3 +1,3 @@
 export default function Texthl() {
-    return (<div className="divboardtitle"><h2 className="textboardhl">H2L ENGENHARIA<hr className="hrr"></hr></h2></div>)
+    return (<div className="divboardtitle2"><h2 className="textboardhl">H2L ENGENHARIA<hr className="hrr"></hr></h2></div>)
 }
