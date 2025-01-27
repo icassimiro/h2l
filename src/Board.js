4682e3633355b9e61d7abe6ea87853d4e211c187
icassimiro <icassimiro@hotmail.com>
2025-01-27 11:59:30 -0300

    css

--- src/Board.js
+++ src/Board.js
@@ -6,11 +6,11 @@ export default function Board() {
             
             <div className="boards">
                 <h2 className="titleboard">Excelência </h2>
-                <h2 className="subboards">Contamos com uma equipe de profissionais altamente qualificados e comprometidos com entregas de qualidade.</h2>
+                <h2 className="subboards">Contamos com uma equipe de <strong className="s">profissionais</strong> altamente <strong className="s">qualificados</strong> e comprometidos com entregas de qualidade.</h2>
             </div>
             <div className="boards">
                 <h2 className="titleboard">Integridade</h2>
-                <h2 className="subboards">Prezamos pela responsabilidade e honestidade, sempre comprometidos com a transparência em tudo o que fazemos.</h2>
+                <h2 className="subboards">Prezamos pela <strong className="s">responsabilidade</strong> e honestidade, sempre comprometidos com a <strong className="s">transparência</strong> em tudo o que fazemos.</h2>
             </div>
     
         
--- src/Boardt.js
+++ src/Boardt.js
@@ -4,7 +4,7 @@ export default function Boardt() {
             <div className="boards">
                 <h2 className="titleboard">Inovação</h2>
                 
-                <h2 className="subboards">Investimos em tecnologia de ponta para oferecer soluções eficientes diante dos desafios de cada empreendimento.</h2>
+                <h2 className="subboards">Investimos em <strong className="s">tecnologia de ponta</strong> para oferecer <strong className="s">soluções eficientes</strong> diante dos desafios de cada empreendimento.</h2>
             </div>
             <div className="boards">
                 <h2 className="titleboard">Sustentabilidade</h2>
