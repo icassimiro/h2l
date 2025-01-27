7d76d3a28f48d165206c7749b56a13b2a93739d4
icassimiro <icassimiro@hotmail.com>
2025-01-27 10:55:15 -0300

    css

--- src/Habtext.js
+++ src/Habtext.js
@@ -9,11 +9,11 @@ export default function Habtext() {
         <h2 className="textboardhab">H2L Engenharia<hr className="hrr"></hr></h2> 
        
            
-            <h3 className="texthab">Fundada em 2016, a H2L Engenharia é referência em soluções inovadoras e sustentáveis no mercado da construção civil. Atuando no Rio de Janeiro e São Paulo, entregamos excelência técnica, eficiência operacional e cumprimento rigoroso de prazos.<hr className="hrhl"></hr></h3>
+            <h3 className="texthab"><strong className="s">Fundada em 2016</strong>, a H2L Engenharia é referência em <strong className="s">soluções inovadoras e sustentáveis</strong> no mercado da construção civil. Atuando no <strong className="s">Rio de Janeiro</strong>e <strong className="s">São Paulo</strong>, entregamos excelência técnica, eficiência operacional e cumprimento rigoroso de prazos.<hr className="hrhl"></hr></h3>
        
     
          
-            <h3 className="texthab">Mais do que projetos, construímos parcerias sólidas com incorporadoras, promovendo impacto positivo nas comunidades e gerando valor para nossos parceiros. Com inovação tecnológica, práticas sustentáveis e uma equipe experiente, transformamos desafios em oportunidades e concretizamos ideias!<hr className="hrhl"></hr></h3>
+            <h3 className="texthab">Mais do que projetos, construímos <strong className="s">parcerias sólidas</strong> com incorporadoras, promovendo <strong className="s">impacto positivo nas comunidades</strong> e gerando valor para nossos parceiros. Com inovação tecnológica, práticas sustentáveis e uma <strong className="s">equipe experiente</strong>, transformamos desafios em oportunidades e concretizamos ideias!<hr className="hrhl"></hr></h3>
       
           
          
