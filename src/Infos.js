609e35675ea716ee8e7305a6026b4b5658579bbb
icassimiro <icassimiro@hotmail.com>
2025-01-28 08:54:27 -0300

    css

--- src/Infos.js
+++ src/Infos.js
@@ -13,7 +13,8 @@ export default function Infos() {
             <div className="infomargin">
                 <div className="divinfo">
                     <h2 className="siga">Siga-nos</h2>
-                    <h2 className="infos"><strong className="s">END:</strong> Rua Marquesa de Santos 217  São Paulo - SP<hr className="hrri"></hr></h2>
+                    <h2 className="infos"><strong className="s">MATRIZ:</strong> Rio de Janeiro - RJ<hr className="hrri"></hr></h2>
+                    <h2 className="infos"><strong className="s">FILIAL:</strong> Rua Marquesa de Santos 217  São Paulo - SP<hr className="hrri"></hr></h2>
                     <h2 className="infos"><strong className="s">CEP:</strong> 04269-040<hr className="hrri"></hr></h2>
                     <h2 className="infos"><strong className="s">TEL:</strong> (21)99384-1466<hr className="hrri"></hr></h2>
                     <h2 className="infos"><strong className="s">E-MAIL:</strong> h2lengenharia@gmail.com<hr className="hrri"></hr></h2>
